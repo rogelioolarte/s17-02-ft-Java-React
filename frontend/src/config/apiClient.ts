f98127361a20d387592946f5bf8cf4b646b09af5
Rogelio Olarte <rogelioolarte2222@gmail.com>
2024-09-09 12:53:35 -0500

    Update apiClient.ts

--- frontend/src/config/apiClient.ts
+++ frontend/src/config/apiClient.ts
@@ -1,14 +1,21 @@
 import axios from 'axios';
 import { MAIN_API } from '../config/routes_api';
+import { useUserActions } from '../hooks/useUserActions';
 
 const apiClient = axios.create({
   baseURL: MAIN_API,
   headers: {
     'Content-Type': 'application/json',
-    'access-control-allow-origin': 'http://localhost:5173'
-    // Otros encabezados comunes que quieras agregar, como Authorization
   },
-  withCredentials: true, // Si tu servidor requiere credenciales de origen cruzado
+  withCredentials: true,
 });
+
+apiClient.interceptors.request.use((config) => {
+    const { user } = useUserActions()
+    if (user.token) {
+      config.headers.Authorization = `Bearer ${user.token}`;
+    }
+    return config;
+  });
 
 export default apiClient;
